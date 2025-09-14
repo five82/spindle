2b0ec202a5e1818f805ab8a282122260bba244f4
five82 <five82@users.noreply.github.com>
2025-09-13 22:51:01 -0400

    removed old queue module

--- src/spindle/disc/rip_spec.py
+++ src/spindle/disc/rip_spec.py
@@ -8,8 +8,8 @@ from typing import Any
 from spindle.disc.metadata_extractor import EnhancedDiscMetadata
 from spindle.disc.monitor import DiscInfo
 from spindle.disc.ripper import Title
-from spindle.queue.manager import QueueItem
 from spindle.services.tmdb_impl import MediaInfo
+from spindle.storage.queue import QueueItem
 
 
 @dataclass
