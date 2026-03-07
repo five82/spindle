f202116e975da5b61bac729bc8e37613c2435f3e
five82 <five82@users.noreply.github.com>
2026-03-07 17:57:37 -0500

    Removed old codebase

--- internal/textutil/ternary.go
+++
@@ -1,9 +0,0 @@
-package textutil
-
-// Ternary is a generic conditional helper that returns a if cond is true, b otherwise.
-func Ternary[T any](cond bool, a, b T) T {
-	if cond {
-		return a
-	}
-	return b
-}
